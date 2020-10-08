3675a090cbbb5379aece732e366feb9eef6e7933
joshyindocosmo <joshy.p@indocosmo.in>
2020-10-08 16:51:17 +0530

    unused import removed

--- src/main/java/com/indocosmo/mmp/controller/CustomerController.java
+++ src/main/java/com/indocosmo/mmp/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.indocosmo.mmp.controller;
 
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
